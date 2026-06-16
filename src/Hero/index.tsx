399f4d059d0e37c75f3af03f0125c36d6b438a09
buyandelgerts <btsendsuren@miu.edu>
2026-06-16 12:37:03 -0500

    renewd cv

--- src/Hero/index.tsx
+++ src/Hero/index.tsx
@@ -32,7 +32,7 @@ const Hero: React.FC<HeaderProps> = ({ handleScroll }) => (
         View My Work
       </button>
       <a
-        href="https://drive.google.com/file/d/1pUo0f1iElUesT1_wJ15xoWVZc909S8L0/view"
+        href="https://drive.google.com/file/d/1CO8gx37qHm0hNc-dG7zWBtdItxQdKjgu/view"
         target="_blank"
         rel="noopener noreferrer"
         className="font-semibold text-white hover:text-pink-400 transition-colors"
